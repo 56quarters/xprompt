e1f95b6e79c5eddb765b267b948360c09c0ea4ee
Nick Pillitteri <nick@tshlabs.org>
2020-10-27 21:37:50 -0400

    Start of git info

--- src/bin/xprompt.rs
+++ src/bin/xprompt.rs
@@ -3,17 +3,26 @@
 //
 //
 
-use std::env;
-use std::fmt::Debug;
-use std::path::PathBuf;
-use std::process;
-use std::time;
-
 use ansi_term::{Color, Style};
-use chrono::{DateTime, Local};
+use chrono::Local;
 use clap::Clap;
-use git2::Repository;
+use git2::{BranchType, Repository};
+use std::env;
+use std::fmt::{Debug, Write};
 
+///
+#[derive(Debug, Clap)]
+#[clap(name = "xprompt")]
+struct PrompterOptions {}
+
+enum GitFlags {
+    UNVERSIONED,
+    MODIFED,
+    ADDED,
+    STASHED,
+}
+
+#[allow(dead_code)]
 struct Pallet {
     black: Style,
     blue: Style,
@@ -45,10 +54,6 @@ impl Default for Pallet {
     }
 }
 
-#[derive(Debug, Clap)]
-#[clap(name = "xprompt")]
-struct PrompterOptions {}
-
 fn get_timestamp() -> String {
     Local::now().format("%Y-%m-%dT%H:%M:%S").to_string()
 }
@@ -62,22 +67,19 @@ fn get_home() -> Option<String> {
 }
 
 fn get_current_dir() -> Option<String> {
-    env::var("PWD").ok().or_else(|| {
-        env::current_dir()
-            .ok()
-            .and_then(|p| p.to_str().map(|s| s.to_owned()))
-    })
+    env::var("PWD")
+        .ok()
+        .or_else(|| env::current_dir().ok().and_then(|p| p.to_str().map(|s| s.to_owned())))
 }
 
 fn get_relative_dir(home: &Option<String>, current: &Option<String>) -> Option<String> {
-
     if let Some(h) = home {
         if let Some(c) = current {
             return Some(if c.starts_with(h) {
                 c.replace(h, "~")
             } else {
                 c.to_owned()
-            })
+            });
         }
     }
 
@@ -86,6 +88,18 @@ fn get_relative_dir(home: &Option<String>, current: &Option<String>) -> Option<S
 
 fn get_host() -> Option<String> {
     gethostname::gethostname().to_str().map(|s| s.to_string())
+}
+
+fn get_git_branch(repo: &Repository) -> Option<String> {
+    if let Ok(r) = repo.head() {
+        r.shorthand().map(|s| s.to_owned())
+    } else {
+        None
+    }
+}
+
+fn get_git_flags<'a>(repo: &'a Repository) -> Vec<GitFlags> {
+    Vec::new()
 }
 
 fn main() {
@@ -97,31 +111,46 @@ fn main() {
     let host = get_host();
     let relative = get_relative_dir(&home, &path);
 
-    // TODO: obviously this should be optional
-    //let repo = Repository::discover(&path).ok();
+    let repo = path.and_then(|p| Repository::discover(p).ok());
+    let mut git_branch = None;
+    let mut git_flags = Vec::new();
 
-    /*
-    let statuses = repo.statuses(None).unwrap();
-    let state = repo.state();
-    for s in statuses.iter() {
-        //println!("status {}: {:?}", s.path().unwrap_or(""), s.status());
+    if let Some(r) = &repo {
+        git_branch = get_git_branch(r);
+        git_flags = get_git_flags(r);
     }
-     */
 
     let pallet = Pallet::default();
 
-    print!(
-        "{timestamp} {as_} {user} {at} {host} {in_} {dir} {on} {git}",
+    let mut buf = String::new();
+    let _ = write!(
+        &mut buf,
+        "{timestamp} {as_} {user} {at} {host} {in_} {dir}",
         timestamp = pallet.cyan.paint(timestamp),
         as_ = pallet.white.paint("as"),
         user = pallet.blue.paint(user.unwrap_or("[unknown]".to_owned())),
         at = pallet.white.paint("at"),
         host = pallet.orange.paint(host.unwrap_or("[unknown]".to_owned())),
         in_ = pallet.white.paint("in"),
-        dir = pallet
-            .green
-            .paint(relative.unwrap_or("[unknown]".to_owned())),
-        on = pallet.white.paint("in"),
-        git = pallet.violet.paint("[git!]"),
-    )
+        dir = pallet.green.paint(relative.unwrap_or("[unknown]".to_owned())),
+    );
+
+    if let Some(b) = git_branch {
+        let _ = write!(
+            &mut buf,
+            " {on} {branch}",
+            on = pallet.white.paint("on"),
+            branch = pallet.violet.paint(b),
+        );
+    }
+
+    // if !git_flags.is_empty() {
+    //     let _ = write!(
+    //         &mut buf,
+    //         " [{flags}]",
+    //         flags = pallet.blue.paint(git_flags.join("")),
+    //     );
+    // }
+
+    print!("{}", buf);
 }
